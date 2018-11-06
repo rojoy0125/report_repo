a351c024fbc8e4929d9a276575b9dfef78b424d8
rojoy0125 <44563910+rojoy0125@users.noreply.github.com>
2018-11-06 20:46:07 +0800

    HELLO WORLD

--- queennie/src/queennie/Queennie.java
+++ queennie/src/queennie/Queennie.java
@@ -17,6 +17,7 @@ public class Queennie {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("HELLO WORLD!!!");
     }
     
 }
